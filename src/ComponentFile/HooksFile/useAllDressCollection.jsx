1ef917313e76a1025afee41caa0b67ca61da0ab4
Fozle Rabbi <fozlerabbi9790@gmail.com>
2023-08-02 00:28:01 +0600

    added ascending and descending feture and extra work (todo)

--- src/ComponentFile/HooksFile/useAllDressCollection.jsx
+++ src/ComponentFile/HooksFile/useAllDressCollection.jsx
@@ -1,20 +1,21 @@
 import { useQuery } from '@tanstack/react-query';
 
-const useAllDressCollection = (getDataObj,selectedOption ) => {
+const useAllDressCollection = (getDataObj, selectedOption, value, selectedOption2) => {
 
-    console.log( selectedOption)
+    console.log(selectedOption2)
+    // console.log(value[1])
     // console.log( getDataObj?.value[0])
     // console.log( getDataObj?.value[1])
     const { data: datas = [], refetch, isLoading } = useQuery({
-        queryKey: ['allDressCollection', getDataObj.itemOffset, getDataObj.endOffset, getDataObj?.value],
+        queryKey: ['allDressCollection', getDataObj.itemOffset, getDataObj.endOffset, value, selectedOption, selectedOption2],
         queryFn: async () => {
-            const res = await fetch(`http://localhost:5000/allDressCollection?itemOffset=${getDataObj.itemOffset}&endOffset=${getDataObj.endOffset}&selectedOption=${selectedOption}`)
+            const res = await fetch(`http://localhost:5000/allDressCollection?itemOffset=${getDataObj.itemOffset}&endOffset=${getDataObj.endOffset}&selectedOption=${selectedOption}&lowRangeValue=${value[0]}&highRangeValue=${value[1]}&ascenDescen=${selectedOption2}`)
             // const res = await fetch(`http://localhost:5000/allDressCollection?itemOffset=${getDataObj.itemOffset}&endOffset=${getDataObj.endOffset}&rangeStart=${getDataObj.value[0]}&rangeEnd=${getDataObj.value[1]}`)
-                return res.json();
-            },
-        })
-        console.log(datas)
-        return [datas.result, datas?.productLength, refetch, isLoading];
+            return res.json();
+        },
+    })
+    // console.log(datas)
+    return [datas.result, datas?.productLength, refetch, isLoading];
 
 };
 
--- src/ComponentFile/PagesFile/HomeFile/AllDressCollection/AllDressCollection.jsx
+++ src/ComponentFile/PagesFile/HomeFile/AllDressCollection/AllDressCollection.jsx
@@ -11,19 +11,28 @@ import 'react-range-slider-input/dist/style.css';
 
 const AllDressCollection = () => {
     const [getData, setGetData] = useState({})
-    const [value, setValue] = useState([30, 60]);
+    const [value, setValue] = useState([0, 150]);
     const [selectedOption, setSelectedOption] = useState("All")
-    const [datas, productLength, refetch, isLoading] = useAllDressCollection(getData, selectedOption);
+    const [selectedOption2, setSelectedOption2] = useState("ascending ")
+    const [datas, productLength, refetch, isLoading] = useAllDressCollection(getData, selectedOption, value, selectedOption2);
     const [productNumber, setProductNumber] = useState(0);
     const [currentPage, setCurrentPage] = useState(0);
+    const [phoductDynamicNumber, setphoductDynamicNumber] = useState(0)
 
     useEffect(() => {
         refetch()
-    }, [getData, selectedOption])
+        setphoductDynamicNumber(productLength)
+    }, [getData, selectedOption, value, productLength, selectedOption2])
 
 
     const itemsPerPage = 12;
     const pageCount = Math.ceil(productNumber / itemsPerPage);
+    let newPageCount
+    if (productLength) {
+        newPageCount = Math.ceil(productNumber / itemsPerPage);
+        // newPageCount = Math.ceil(phoductDynamicNumber / itemsPerPage);
+    }
+    // const pageNumber = [...Array(newPageCount).keys()]
     const pageNumber = [...Array(pageCount).keys()]
 
     useEffect(() => {
@@ -44,19 +53,24 @@ const AllDressCollection = () => {
         setSelectedOption(e.target.value)
     }
 
+    const handleOptionChange2 = (e) => {
+        setSelectedOption2(e.target.value)
+    }
+
     return (
         <div className='mt-16'>
             <h2 className='text-center text-2xl font-bold text-color pb-10'>All Cullection</h2>
             <div className='flex'>
-                <div className="conterol-div w-2/12 px-2">
+                <div className="conterol-div w-2/12 pe-3">
                     <h2 className='text-center mb-4 text-color text-xl font-bold'>control panel</h2>
                     <div className='my-2'>
-                        <h2>total Product = {productLength}</h2>
+                        <h2>total Product = {productNumber}</h2>
+                        <h2>Find Product = {productLength}</h2>
                         <h2 >total Product = {datas?.length} / page</h2>
                     </div>
 
                     <div>
-                        <RangeSlider min={0} max={120} step={5} onInput={setValue} />
+                        <RangeSlider min={0} max={150} step={5} onInput={setValue} />
                         <div className='flex justify-between mt-2'>
                             <span> {value[0]} $</span>
                             <span> --- </span>
@@ -64,38 +78,68 @@ const AllDressCollection = () => {
                         </div>
                     </div>
 
-                    <select
-                        value={selectedOption}
-                        onChange={handleOptionChange}
-                        className="bg-green-400  mt-2 button rounded  text-gray-800"
-                    >
-                        <option className='ms-2' value="All">All</option>
-                        <option className='ms-2' value="Boy">Boy</option>
-                        <option className='ms-2' value="Girl">Girl</option>
-                    </select>
+                    <div className='flex justify-between mt-2'>
+                        <select
+                            value={selectedOption}
+                            onChange={handleOptionChange}
+                            className="bg-green-400  mt-2 button rounded  text-gray-800"
+                        >
+                            <option className='ms-2' value="All">All</option>
+                            <option className='ms-2' value="Boy">Boy</option>
+                            <option className='ms-2' value="Girl">Girl</option>
+                        </select>
 
+                        <select
+                            // value={selectedOption2}
+                            onChange={handleOptionChange2}
+                            className="bg-green-400  mt-2 button rounded  text-gray-800"
+                        >
+                            <option className='ms-2' value="ascending ">Low to high</option>
+                            <option className='ms-2' value="descending ">High to low</option>
+                        </select>
+
+                    </div>
 
 
                 </div>
                 <div className=' w-10/12'>
                     {
-                        isLoading || datas.length === 0 ? <div className='all-dress-mainDiv grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-10 '><SkeletonCard cards={itemsPerPage}></SkeletonCard></div> :
-                            <div className="all-dress-mainDiv grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-10 ">
-
+                        isLoading ? <div className='all-dress-mainDiv grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-10 '><SkeletonCard cards={itemsPerPage}></SkeletonCard></div> :
+                            <div>
                                 {
-                                    datas?.map(data =>
-                                        <SingleDress
-                                            key={data._id}
-                                            data={data}
-                                        ></SingleDress>
-                                    )
+                                    datas?.length === 0 ? <p className='text-red-500 text-center my-44 text-3xl font-semibold'>No More Data Back to Previous Page</p> :
+                                        <div className="all-dress-mainDiv grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-10 ">
+
+                                            {
+                                                datas?.map(data =>
+                                                    <SingleDress
+                                                        key={data._id}
+                                                        data={data}
+                                                    ></SingleDress>
+                                                )
+                                            }
+
+                                        </div>
+
                                 }
                             </div>
+                        // <div className="all-dress-mainDiv grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-10 ">
+
+                        //     {
+                        //         datas?.map(data =>
+                        //             <SingleDress
+                        //                 key={data._id}
+                        //                 data={data}
+                        //             ></SingleDress>
+                        //         )
+                        //     }
+
+                        // </div>
                     }
 
                     <ul className='flex gap-10 mx-auto w-6/12 justify-center my-10'>
                         {
-                            pageNumber?.map(pageNum =>
+                            pageNumber && pageNumber?.map(pageNum =>
                                 <li onMouseUp={() => getDataFun(pageNum)} onClick={() => setCurrentPage(pageNum)} className={`${pageNum === currentPage ? "active-pagination " : ""} border cursor-pointer bg-gray-300 px-2`} key={pageNum}> {pageNum + 1} </li>
                             )
                         }
